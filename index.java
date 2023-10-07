2584c6bfe0321d8c91391606f40ff80059647a7d
Orzibekov Yusufbek <orzibekovyusufbek@gmail.com>
2023-10-08 00:37:33 +0500

    Created index.java file

---
+++ index.java
@@ -0,0 +1,9 @@
+public class FrameOutput {
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.println("*   *   *\t");
+        System.out.println("*   *   *\t");
+        System.out.println("*   *   *\t");
+        System.out.println("*********");
+    }
+}
